8ce234b4dc2816870a82df8d5fad43eba16aa0e5
tingalbert0 <60828424+tingalbert0@users.noreply.github.com>
2020-03-14 19:11:53 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,38 +8,38 @@ client.on('ready', () => {
 
 client.on('message', message => {
     var times = SunCalc.getTimes(new Date(), 33.7940, -84.3889);
-	var sunset = times.sunset.getHours() + ':' + times.sunset.getMinutes();
-	var isDinnerTime = new Date().getHours() == times.sunset.getHours() && new Date().getMinutes() == times.sunset.getMinutes();//exactly at sunset
-	var nowHours = new Date().getHours();
-	var nowMinutes = new Date().getMinutes();
-	var now = nowHours + ':' + nowMinutes;
-	var EatingTime = false;
+    var sunset = times.sunset.getHours() + ':' + times.sunset.getMinutes();
+    var isDinnerTime = new Date().getHours() == times.sunset.getHours() && new Date().getMinutes() == times.sunset.getMinutes();//exactly at sunset
+    var nowHours = new Date().getHours();
+    var nowMinutes = new Date().getMinutes();
+    var now = nowHours + ':' + nowMinutes;
+    var EatingTime = false;
     if (nowHours == times.sunset.getHours()) {
-		if (nowMinutes <= times.sunset.getMinutes()) {
-			EatingTime = false;
-		} else {
-			EatingTime = true;
-		}
-	} else if (nowHours > times.sunset.getHours()) {
-		EatingTime = true;
+	if (nowMinutes <= times.sunset.getMinutes()) {
+	    EatingTime = false;
+	} else {
+	    EatingTime = true;
 	}
+    } else if (nowHours > times.sunset.getHours()) {
+	EatingTime = true;
+    }
 	
-	if (nowHours == times.sunrise.getHours()) {
-		if (nowMinutes <= times.sunrise.getHours()) {
-			EatingTime = true;
-		} else {
-			EatingTime = false;
-		}
-	} else if (nowHours < times.sunrise.getHours()) {
-		EatingTime = true;
+    if (nowHours == times.sunrise.getHours()) {
+	if (nowMinutes <= times.sunrise.getHours()) {
+	    EatingTime = true;
+	} else {
+	    EatingTime = false;
 	}
+    } else if (nowHours < times.sunrise.getHours()) {
+	EatingTime = true;
+    }
     
     if (message.content === 'can i eat now') {
        	if (EatingTime) {
-			bot.sendMessage({to: channelID, message: 'Yes <:hypers:650866330005798912>'});
-		} else {
-			bot.sendMessage({to: channelID, message: 'No <:PepeHands:685227664776101912>'});
-		}
+	    message.reply('Yes <:hypers:650866330005798912>');
+	} else {
+	    message.reply('No <:PepeHands:685227664776101912>');
+	}
     }
 });
 

23c8bd2e0f01076c8e90e2a5cfa9cc4a06ec3473
tingalbert0 <60828424+tingalbert0@users.noreply.github.com>
2020-03-14 19:04:11 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,46 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const suncalc = require('suncalc');
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-       message.reply('pong');
-       }
+    var times = SunCalc.getTimes(new Date(), 33.7940, -84.3889);
+	var sunset = times.sunset.getHours() + ':' + times.sunset.getMinutes();
+	var isDinnerTime = new Date().getHours() == times.sunset.getHours() && new Date().getMinutes() == times.sunset.getMinutes();//exactly at sunset
+	var nowHours = new Date().getHours();
+	var nowMinutes = new Date().getMinutes();
+	var now = nowHours + ':' + nowMinutes;
+	var EatingTime = false;
+    if (nowHours == times.sunset.getHours()) {
+		if (nowMinutes <= times.sunset.getMinutes()) {
+			EatingTime = false;
+		} else {
+			EatingTime = true;
+		}
+	} else if (nowHours > times.sunset.getHours()) {
+		EatingTime = true;
+	}
+	
+	if (nowHours == times.sunrise.getHours()) {
+		if (nowMinutes <= times.sunrise.getHours()) {
+			EatingTime = true;
+		} else {
+			EatingTime = false;
+		}
+	} else if (nowHours < times.sunrise.getHours()) {
+		EatingTime = true;
+	}
+    
+    if (message.content === 'can i eat now') {
+       	if (EatingTime) {
+			bot.sendMessage({to: channelID, message: 'Yes <:hypers:650866330005798912>'});
+		} else {
+			bot.sendMessage({to: channelID, message: 'No <:PepeHands:685227664776101912>'});
+		}
+    }
 });
 
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
